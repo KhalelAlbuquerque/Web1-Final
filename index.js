70b962b66bde365c4345c33aba0f77d40d58ba4f
KhalelAlbuquerque <khaleledipo@gmail.com>
2023-06-13 19:21:19 -0300

    First db design

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 
+
 const app = express()
 
 
+// Ajustando view engine para o handlebars
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-conn.sync().then(()=>{
+app.use(express.static('public'))
+
+const UserSchema = require('./models/User')
+const PostSchema = require('./models/Post')
+
+
+conn
+.sync({force:true})
+.then(()=>{
     app.listen(3000)
     console.log("Banco de dados ok")
-})
+}) 
