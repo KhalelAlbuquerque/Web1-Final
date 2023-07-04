1579af48980f7999be214e3910013d99afad48dd
Khalel Albuquerque <khaleledipo@gmail.com>
2023-07-04 01:27:07 -0300

    Feats: Post controller, create e get posts

---
+++ controllers/postController.js
@@ -0,0 +1,46 @@
+const postSchema = require('../db/models/Post')
+
+module.exports = class postController{
+
+    static async getPosts(req,res){
+
+        const posts = await postSchema.findAll()
+        
+        return res.status(200).json({message:"Posts encontrados"})
+    }
+
+    static async findPost(req, res){
+
+        const id = req.params.id
+
+        const post = await postSchema.findOne({where: {id:id}})
+
+        return res.status(200).json({message:"Post encontrado"})
+
+    }
+
+    static async createPostPost(req,res){
+
+        const {title, description, startDate, endDate} = req.body
+        const userid = req.session.userid
+
+        try{
+            const postData = {
+                title,
+                description,
+                startDate,
+                endDate,
+                UserId:userid
+            }
+
+            await postSchema.create(postData)
+            
+            return res.status(201).json({message:"Post criado com sucesso"})
+
+        }catch(err){
+            return res.status(500).json({message:err.message})
+        }
+
+    }
+
+}
--- index.js
+++ index.js
@@ -15,6 +15,32 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(session({
+    name:'session',
+    saveUninitialized:false,
+    secret:"senha",
+    resave:false,
+    cookie:{
+        secure:false,
+        maxAge:60*60*1000,
+        httpOnly:true,
+        expires: new Date(Date.now+60*60*1000)
+    }
+}))
+
+
+app.use((req,res,next)=>{
+
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+
+    next()
+
+})
+
+
+
 const UserSchema = require('./db/models/User')
 const PostSchema = require('./db/models/Post')
 
